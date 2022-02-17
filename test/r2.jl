016401a68026be1906bd298b27d2a3d4783fc5e5
Phillip Alday <me@phillipalday.com>
2022-02-17 09:58:08 -0600

    YASG

--- test/r2.jl
+++ test/r2.jl
@@ -13,12 +13,12 @@ end
 progress = false
 
 @testset "LMM" begin
-    model = fit(MixedModel, @formula(reaction ~ 1 + (1|subj)),
+    model = fit(MixedModel, @formula(reaction ~ 1 + (1 | subj)),
                 dataset(:sleepstudy); progress)
 
-    @test r2(model;conditional=true) ≈ cor(response(model), fitted(model))^2
-    @test r2(model;conditional=false) ≈ cor(response(model), model.X * model.β)^2
+    @test r2(model; conditional=true) ≈ cor(response(model), fitted(model))^2
+    @test r2(model; conditional=false) ≈ cor(response(model), model.X * model.β)^2
 
-    @test adjr2(model;conditional=true) ≈ _adjust(r2(model; conditional=true), model)
-    @test adjr2(model;conditional=false) ≈ _adjust(r2(model; conditional=false), model)
+    @test adjr2(model; conditional=true) ≈ _adjust(r2(model; conditional=true), model)
+    @test adjr2(model; conditional=false) ≈ _adjust(r2(model; conditional=false), model)
 end
