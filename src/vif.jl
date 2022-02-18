72df44bc327e723cba41ddb50266f413e7c47f28
Phillip Alday <me@phillipalday.com>
2022-02-17 22:41:45 -0600

    docstrings

--- src/vif.jl
+++ src/vif.jl
@@ -1,7 +1,3 @@
-# I think car computes GVIF combining all the column-wise VIFs
-# for a single categorical predictor, so that's something
-# to look into
-
 _rename_intercept(s) = strip(s) == "1" ? "(Intercept)" : s
 
 """
@@ -32,7 +28,11 @@ Returns a vector of inflation factors computed for each coefficient except
 for the intercept.
 In other words, the corresponding coefficients are `coefnames(m)[2:end]`.
 
-See also [`coefnames`](@ref).
+The [variance inflation factor (VIF)](https://en.wikipedia.org/wiki/Variance_inflation_factor) measures
+the increase in the variance of a parameter's estimate in a model with multiple parameters relative to
+the variance in of a paremeter's estimate in a model containing only that parameter.
+
+See also [`coefnames`](@ref), [`gvif`](@ref).
 
 !!! warning
     This method will fail if there is (numerically) perfect multicollinearity,
@@ -48,8 +48,15 @@ function vif(m::RegressionModel)
         throw(ArgumentError("VIF not meaningful for models with only one non-intercept term"))
     # directly computing inverses is bad, but
     # generally this shouldn't be a huge matrix and it's symmetric
-    # NB: The coorrelation matrix is positive definite and hence invertible
+    # NB: The correlation matrix is positive definite and hence invertible
     #     unless there is perfect rank deficiency, hence the warning.
+    # NB: The determinate technique for GVIF could also be applied
+    #     columnwise (instead of Term-wise) here, but it wouldn't really
+    #     be any more efficient because this is a Symmetric matrix and computing
+    #     all those determinants has its cost. The determinants also hint at
+    #     how you could show equivalency, if you remember that inversion is solving
+    #     a linear system and that Cramer's rule -- which uses determinants --
+    #     can also a linear system
     return diag(inv(Symmetric(mm)))
 end
 
@@ -65,12 +72,25 @@ Returns a vector of inflation factors computed for each term except
 for the intercept.
 In other words, the corresponding coefficients are `termnames(m)[2:end]`.
 
-See also [`termnames`](@ref).
+The [generalized variance inflation factor (VIF)](https://doi.org/10.2307/2290467)
+measuresthe increase in the variance of a (group of) parameter's estimate in a model
+with multiple parameters relative to the variance in of a paremeter's estimate in a
+model containing only that parameter. For continuous, numerical predictors, the GVIF
+is the same as the VIF, but for categorical predictors, the GVIF provides a singule
+number for the entire group of contrast-coded coefficients associated with a categorical
+predictor.
+
+See also [`termnames`](@ref), [`vif`](@ref)..
 
 !!! warning
     This method will fail if there is (numerically) perfect multicollinearity,
     i.e. rank deficiency (in the fixed effects). In that case though, the VIF
     isn't particularly informative anyway.
+
+## References
+
+Fox, J., & Monette, G. (1992). Generalized Collinearity Diagnostics.
+Journal of the American Statistical Association, 87(417), 178. doi:10.2307/2290467
 """
 function gvif(m::RegressionModel; scaled_by_df=false)
     mm = StatsBase.cov2cor!(vcov(m), stderror(m))
