25b70b722d06a0159ce687da3a216acebe940f86
Phillip Alday <me@phillipalday.com>
2022-09-15 14:48:26 +0200

    docstring

--- src/remef.jl
+++ src/remef.jl
@@ -2,13 +2,41 @@
 # Intercept always has to be specified
 """
     partial_fitted(model::LinearMixedModel,
-                        fe::AbstractVector{<:AbstractString},
-                        re::Dict{Symbol}=Dict(fn => fe for fn in fnames(model));
-                        mode=:include)
+                   fe::AbstractVector{<:AbstractString},
+                   re::Dict{Symbol}=Dict(fn => fe for fn in fnames(model));
+                   mode=:include)
 
 Compute "partial" fitted values.
 
-Partial fitted values are useful for computing partial residuals.
+Partial fitted values are useful for computing partial residuals. They are the
+fitted values obtained by setting selected model terms to zero, while preserving
+the other values at their original estimates.
+
+The fixed effects coefficients to use (`fe`) are specified as vector of strings.
+For specifying no coefficients, use the empty string vector `String[]`.
+
+The random effects `re` are specified as a dictionary, with the grouping variables
+as keys and the vector of group-level coefficients specified as vectors.
+For example, `Dict(:subj => ["(Intercept)"])` specifies that `(1|subj)` should
+be kept.
+The default is to match the specified fixed effects for all grouping variables,
+but note that this will fail when the fixed effects specification is incompatible
+with any grouping variable.
+
+The keyword arugment `mode` specifies whether the fixed and random effects
+specifications are treated as coefficients to `:include` or `:exclude`.
+
+!!! note
+    For both the fixed and the random effects, the relevant entities are the
+    coefficient names, not the original term names.
+
+!!! warning
+    The intercept is **not** automatically / implicitly included and must
+    always be explicitly specified.
+
+!!! warning
+    This functionality has not been tested on and thus verified to work with
+    models with rank-deficient fixed effects.
 
 This functionality is similar to the [remef](https://github.com/hohenstein/remef) package in R.
 """
