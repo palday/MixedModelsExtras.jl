a70cc5db60e3bcdb83437505d698c3f614c2bf0f
Phillip Alday <palday@users.noreply.github.com>
2023-09-13 06:24:50 -0500

    use StatsModels.[g]vif (#22)
    * use StatsModels.[g]vif
    
    * compat
    
    * julia compat floor
    
    * remove unused file

--- src/MixedModelsExtras.jl
+++ src/MixedModelsExtras.jl
@@ -7,14 +7,16 @@ using StatsBase
 using StatsModels
 using Tables
 
+using StatsModels: termnames, vif, gvif
+export termnames, gvif, vif
+
+StatsModels.termnames(::RandomEffectsTerm) = String[]
+
 include("icc.jl")
 export icc
 
 include("r2.jl")
 export r², r2, adjr², adjr2
-
-include("vif.jl")
-export termnames, gvif, vif
 
 include("remef.jl")
 export partial_fitted
